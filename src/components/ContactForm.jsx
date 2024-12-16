aff45a12ea03a722115e7eb502941ccf6980ee07
nishantkaneria <nishant.k@7span.com>
2024-12-16 15:44:21 +0530

    design set

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -6,10 +6,10 @@ const ContactForm = () => {
       <div className="flex gap-24">
         <div className="flex-1">
           <div>
-            <h2 class="mb-4 uppercase text-4xl tracking-tight font-bold text-gray-900">
+            <h2 className="mb-4 uppercase text-4xl tracking-tight font-bold text-gray-900">
               Contact Us
             </h2>
-            <p class="mb-8 lg:mb-16 font-light text-black sm:text-base">
+            <p className="mb-8 lg:mb-16 font-light text-black sm:text-base">
               Got a technical issue? Want to send feedback about a beta feature?
               Need details about our Business plan? Let us know.
             </p>
@@ -48,70 +48,70 @@ const ContactForm = () => {
             </div>
           </div>
         </div>
-        <div class="flex-1">
-          <form action="#" class="space-y-8">
+        <div className="flex-1">
+          <form action="#" className="space-y-8">
             <div>
               <label
-                for="email"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 capitalize"
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 capitalize"
               >
                 name
               </label>
               <input
                 type="email"
                 id="email"
-                class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
                 placeholder="Enter your name"
                 required
               />
             </div>
             <div>
               <label
-                for="number"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 capitalize"
+                htmlFor="number"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 capitalize"
               >
                 number
               </label>
               <input
                 type="number"
                 id="number"
-                class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500"
+                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500"
                 placeholder="Enter phone number"
                 required
               />
             </div>
             <div>
               <label
-                for="email"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 capitalize"
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 capitalize"
               >
                 your email
               </label>
               <input
                 type="email"
                 id="email"
-                class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
                 placeholder="Enter your email"
                 required
               />
             </div>
-            <div class="sm:col-span-2">
+            <div className="sm:col-span-2">
               <label
-                for="message"
-                class="block mb-2 text-sm font-medium text-gray-900 capitalize"
+                htmlFor="message"
+                className="block mb-2 text-sm font-medium text-gray-900 capitalize"
               >
                 your message
               </label>
               <textarea
                 id="message"
                 rows="6"
-                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 "
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 "
                 placeholder="Leave a comment..."
               ></textarea>
             </div>
             <button
               type="submit"
-              class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 opacity-95 hover:opacity-100"
+              className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 opacity-95 hover:opacity-100"
             >
               Send message
             </button>
